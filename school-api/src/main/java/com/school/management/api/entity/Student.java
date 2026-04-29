73003a4d3bcb4d589de30288ecd7226616d0e309
bijulal <bijulalp199@gmail.com>
2026-04-29 19:16:42 +0530

    created student entity,controller,repository

---
+++ school-api/src/main/java/com/school/management/api/entity/Student.java
@@ -0,0 +1,36 @@
+package com.school.management.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "roll_number", nullable = false, unique = true)
+    private String rollNumber;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name ="gender",nullable = false)
+    private String gender;
+
+    @Column(name = "blood_group",length = 5)
+    private String bloodGroup;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+
+}
