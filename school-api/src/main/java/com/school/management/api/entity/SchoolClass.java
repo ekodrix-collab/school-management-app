6c6470193cc9edbdda0ab144d6b05ced033750d3
Unais-Kuruniyan <unaiskuruniyan@gmail.com>
2026-04-30 16:42:11 +0530

    Develop the complete backend module for managing classes in the school management system. This includes creating the Class entity, repository, service layer, controller, and REST APIs for adding, editing, and deleting classes.

---
+++ school-api/src/main/java/com/school/management/api/entity/SchoolClass.java
@@ -0,0 +1,44 @@
+package com.school.management.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "school_classes")
+public class SchoolClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "standard", nullable = false)
+    private String standard;        // "1", "LKG"
+
+    @Column(name = "division")
+    private String division;      // "A", "B"
+
+    @Column(name = "display_name")
+    private String displayName;  // "1-A"
+
+    @Column(name = "capacity")
+    private Integer capacity;
+
+    // Simple approach (MVP)
+    @Column(name = "class_teacher_id")
+    private Long classTeacherId;
+
+    @Column(name = "academic_year_id")
+    private String academicYearID; // "2025-2026"
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ school-api/src/main/java/com/school/management/api/model/requstModel/SchoolClassRequest.java
@@ -0,0 +1,19 @@
+package com.school.management.api.model.requstModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class SchoolClassRequest {
+    @JsonProperty(value = "standard")
+    private String standard;        // "1", "LKG"
+
+    @JsonProperty(value = "division")
+    private String division;
+
+    @JsonProperty(value = "class_teacher_id")
+    private Long classTeacherId;
+
+
+
+}
