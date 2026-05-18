efa60e080f04a52270955ddc4413e8c9df2c0f66
mrithul <Mridhul@radiantwtech.in>
2026-05-18 09:07:16 +0530

    update code

--- school-api/src/main/java/com/school/management/api/repository/AdmissionRepository.java
+++ school-api/src/main/java/com/school/management/api/repository/AdmissionRepository.java
@@ -4,6 +4,18 @@ import com.school.management.api.entity.Admission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AdmissionRepository extends JpaRepository<Admission,Long> {
+
+    Optional<Admission> findByAdmissionId(String admissionId);
+
+    List<Admission> findAllBySchoolId(String schoolId);
+
+    List<Admission> findAllByAcademicYearIdAndSchoolId(String academicYearId, String schoolId);
+
+    long countByAcademicYearIdAndSchoolId(String academicYearId, String schoolId);
+
 }
